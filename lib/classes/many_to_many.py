18d61577e1b30c48aadf9090393cadab7ce9b383
Wycliffe <WycliffeBenedict25@gmail.com>
2025-04-30 11:39:26 +0300

    SQL-alchemy-lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,123 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of Author.")
+        if not isinstance(magazine, Magazine):
+            raise Exception("Magazine must be an instance of Magazine.")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise Exception("Title must be a string between 5 and 50 characters.")
+
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title 
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise Exception("Author must be an instance of Author.")
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise Exception("Magazine must be an instance of Magazine.")
+        self._magazine = value
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise Exception("Name must be a non-empty string.")
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name 
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
-
+        if not self.articles():
+            return None
+        return list({article.magazine.category for article in self.articles()})
+    
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
+        self._name = None
+        self._category = None
+        self.name = name 
         self.category = category
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception("Name must be a string.")
+        if not (2 <= len(value) <= 16):
+            raise Exception("Name must be between 2 and 16 characters.")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise Exception("Category must be a string.")
+        if len(value.strip()) == 0:
+            raise Exception("Category must be a non-empty string.")
+        self._category = value
+
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        pass
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self.articles():
+            author = article.author
+            author_counts[author] = author_counts.get(author, 0) + 1
+        result = [author for author, count in author_counts.items() if count > 2]
+        return result if result else None
+    
+    def top_publisher(cls):
+        if not Article.all:
+            return None
+        magazine_counts = {}
+        for article in Article.all:
+            magazine = article.magazine
+            magazine_counts[magazine] = magazine_counts.get(magazine, 0) + 1
+        return max(magazine_counts, key=magazine_counts.get)
+ 
